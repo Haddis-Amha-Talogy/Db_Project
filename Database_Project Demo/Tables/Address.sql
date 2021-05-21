5be05add14f80dc30d1c3d0eaa0994366e45f8aa
Haddis Amha <haddis.amha@CORP.OnePSI.net>
2021-05-21 09:50:30 -0400

    Add project files.

---
+++ Database_Project Demo/Tables/Address.sql
@@ -0,0 +1,12 @@
+﻿CREATE TABLE [dbo].[Address]
+(
+	[Id] INT NOT NULL PRIMARY KEY IDENTITY,
+	[PersonID] INT NOT NULL,
+	[Address ] NVARCHAR(50) NULL, 
+    [City] NVARCHAR(50) NULL, 
+    [State] NVARCHAR(50) NULL, 
+    [ZipCode] NVARCHAR(50) NULL, 
+    CONSTRAINT [FK_Address_Person] FOREIGN KEY ([PersonID]) REFERENCES [Person]([Id]), 
+    
+   
+)
---
+++ Database_Project Demo/View/FullPerson.sql
@@ -0,0 +1,6 @@
+﻿CREATE VIEW [dbo].[FullPerson]
+	AS 
+	SELECT [p].[Id] as PersonID, [p].[FirstName], [p].[LastName], [a].[Id], 
+	[a].[Address ], [a].[City], [a].[State], [a].[ZipCode] 
+	FROM dbo.Person p 
+	Left join dbo.Address a ON p.Id = a.PersonID
